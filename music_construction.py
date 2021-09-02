1bf17429741245966b5a6d73738158255c84df18
AwdeDarkar <darkardengeno@gmail.com>
2021-09-02 19:59:10 -0400

    Add chord context

--- music_construction.py
+++ music_construction.py
@@ -1,6 +1,7 @@
 import time
 
 import mingus.core.scales as scales
+import mingus.core.chords as chords
 from mingus.containers import Note, Track, Bar
 from mingus.midi import fluidsynth
 
@@ -17,6 +18,15 @@ class Rhythm:
             def __getitem__(self, _):
                 return meter[1]
         return EvenRhythm()
+
+    @classmethod
+    def Swung(cls, *meter):
+        class SwungRhythm(Rhythm):
+            def __getitem__(self, i):
+                if i%2 == 0:
+                    return meter[1] / 1.5
+                return meter[1] * 2
+        return SwungRhythm()
 
 class Motion:
     def __init__(self, ante, cons, context, rhythm=Rhythm.Even(4,4), fill=[]):
@@ -76,12 +86,45 @@ class Motion:
         return Motion(self.ante, self.cons, context, self.rhythm, self.fill)
 
     def set_equivalent_context(self, context):
-        cante = self.context[self.ante]
-        ccons = self.context[self.cons]
-        return Motion(context.index(cante), context.index(ccons), context, self.rhythm, self.fill)
+        cante = context.index(self.context[self.ante])
+        ccons = context.index(self.context[self.cons])
+        cfill = [context.index(self.context[i]) for i in self.fill]
+        return Motion(cante, ccons, context, self.rhythm, cfill)
 
     def fill_interval(self):
-        return self.set_fill(list(range(self.ante+1, self.cons)))
+        if self.ante < self.cons:
+            return self.set_fill(list(range(self.ante+1, self.cons)))
+        else:
+            return self.set_fill(list(range(self.cons+1, self.ante))[::-1])
+
+    def skip(self, skip):
+        return self.set_fill([v for i, v in enumerate(self.fill) if i not in skip])
+
+
+class MusicalContext:
+    def __getitem__(self, index):
+        """
+        Get the indexed contextual note or chord; must support octave shifting for out-of-bounds
+        """
+        raise NotImplementedError
+
+    def index(self, value):
+        """ Calculate the index of the note or chord given; raise ValueError if it is not in context """
+        raise NotImplementedError
+
+    @property
+    def parent(self):
+        """ Return the 'parent' context, if any """
+        return None
+
+    @property
+    def relatives(self):
+        """
+        Return any relative contexts (chord inversions, extensions, relative majors or minors,
+        etc
+        """
+        return {}
+
 
 class ScaleContext:
     def __init__(self, scale, octave=4):
@@ -91,8 +134,21 @@ class ScaleContext:
     def __getitem__(self, index):
         return f"{self.scale.degree(index % 7 + 1)}-{self.octave + index//7}"
 
+    def index(self, value):
+        note, octave = value.split("-")
+        return self.scale.ascending().index(note) + (7 * (self.octave - int(octave)))
+
+class ChordContext:
+    def __init__(self, chord, octave=4):
+        self.chord = chord
+        self.octave = octave
+
+    def __getitem__(self, index):
+        return f"{self.chord[(index % len(self.chord))]}-{self.octave + index//len(self.chord)}"
+
 if __name__ == "__main__":
     fluidsynth.init("./soundfonts/piano/grand-piano.sf2", driver="alsa")
-    m = Motion(0, 7, ScaleContext(scales.Ionian("C")))
-    m.fill_interval().play(fluidsynth)
-
+    m = Motion(7, 0, ChordContext(chords.triad("C", "D")))
+    m\
+            .fill_interval()\
+            .play(fluidsynth)
